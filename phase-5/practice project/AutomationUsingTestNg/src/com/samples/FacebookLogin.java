fa43e01e3cf5b158acbd31bcbe401a50d480d374
Aman <amankrsonpur4@gmail.com>
2022-09-30 15:07:46 +0530

    practice project

---
+++ phase-5/practice project/AutomationUsingTestNg/src/com/samples/FacebookLogin.java
@@ -0,0 +1,37 @@
+package com.samples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FacebookLogin {
+	
+	WebDriver driver;
+	@Test
+	public void facebook() {
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("aman1234@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Aman123@");
+		driver.findElement(By.name("login")).submit();
+		
+		
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+	
+	
+}
